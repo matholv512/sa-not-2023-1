4f691f2585cb2066699e99580f4dc6fb6a5618a1
Matheus Oliveira <79008689+matholv512@users.noreply.github.com>
2023-03-10 01:07:09 +0000

    (09/03/23) - Finalização de ataque SQL Injection e prevenção

---
+++ xss/public/javascripts/virus.js
@@ -0,0 +1,14 @@
+let normal = false
+
+setInterval(() => {
+    const body = document.body
+    if (normal) {
+        body.style.color = 'black'
+        body.style.backgroundColor = 'white'
+    }
+    else {
+        body.style.color = 'white'
+        body.style.backgroundColor = 'black'
+    }
+    normal = !normal
+}, 1000)
