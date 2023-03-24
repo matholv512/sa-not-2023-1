cde896519ac4304e663e2c961ad2d0d14d051cb9
Matheus Oliveira <79008689+matholv512@users.noreply.github.com>
2023-03-24 01:11:49 +0000

    (23/03/23) - Implementação do cadastro de usuários em auth-session

--- auth-session/routes/users.js
+++ auth-session/routes/users.js
@@ -3,5 +3,8 @@ var router = express.Router();
 const controller = require('../controllers/users')
 
 router.get('/form', controller.formNew)
+router.post('/login', controller.formLogin)
+router.post('/', controller.create)
+router.post('auth', controller.auth)
 
 module.exports = router;
